519688323498c48fdb98bf079ee5b006b330ed6c
sanpreet <sany26.com@gmail.com>
2013-08-01 11:34:57 +0530

    Update Graph.sh
    Line 39 is changed and Graph is more Smooth.

--- Graph.sh
+++ Graph.sh
@@ -1,62 +1,53 @@
-while read line; do
-if [ "$line" != "EOF" ]; then
-echo "$line" >> Sensor.txt
-else
-break
-fi
+while read line; 
+do
+    if [ "$line" != "E" ]; then
+        echo "$line" >> Sensor.txt
+    else
+        break
+    fi
 done < /dev/ttyUSB0
+
 sed -e 's/[\t ]//g;/^$/d' Sensor.txt > Finput.txt
 awk 'NR%2==0' Finput.txt | cat > sensor1.txt
 awk 'NR%2!=0' Finput.txt | cat > sensor2.txt
 g++ main.cpp
-./a.out
-#rm a.out
+    ./a.out
+    rm a.out
 #rm sensor1_out.txt
 #rm sensor1.txt
 #rm senssor2.txt
 g++ processor.cpp
-./a.out 
-#r a.out
+    ./a.out 
+#rm a.out
 #rm X-axis.txt
 #rm Y-axis.txt
-echo "Enter name by which graph is to be saved:"
-read a
-if [ ! -d "Previous" ]; 
-then
-mkdir Previous
-fi
+echo "Enter name of graph:"
+read name
+    if [ ! -d "Previous" ]; 
+    then
+        mkdir Previous
+    fi
 
 /usr/bin/gnuplot  <<_EOF_
-set xrange [-10:50]
-set yrange [310:0]
-set style data linespoints
+set yrange [320:0]
 set terminal svg
-plot 'output.txt'lc rgb 'black'
+plot 'output.txt'lc rgb 'red'
 set xlabel 'Stress'
 set ylabel 'Strain'
 set key off
-plot 'output.txt' 
+set timestamp "%d/%m/%y %H:%M" 80,-2 "Helvetica"
+plot 'output.txt' using 1:2 smooth bezier with lines linecolor rgb "red"
 set title 'Sensor Plot'
 
 	
 set term postscript eps enhanced "Helvetica" 24
-set output '$a.svg'
+set output '$name.svg'
 replot
 _EOF_
 
-mv $a.svg Previous
+mv $name.svg Previous
 cd Previous
-gimp "$a.svg"
+gimp "$name.svg"
 cd ../
-rm output.txt -f
-rm Finputss.txt -f
-set output '$a.svg'
-replot
-_EOF_
-
-mv $a.svg Previous
-cd Previous
-gimp "$a.svg"
-cd ../
-rm output.txt -f
-rm Finputss.txt -f
+#rm output.txt -f
+#rm Finputs.txt -f
